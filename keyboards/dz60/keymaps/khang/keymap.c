31d1144f0b59b4f7fd45bee379244476c5de1c09
nguyenvukhang <brew4k@gmail.com>
2021-05-27 14:56:00 +0800

    moved all obscure keys to HYPR

--- keyboards/dz60/keymaps/khang/keymap.c
+++ keyboards/dz60/keymaps/khang/keymap.c
@@ -23,15 +23,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FL] = LAYOUT_60_ansi_split_backspace(
     MO(_SL),  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NO,    KC_NO,
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_BRID,  KC_BRIU,  KC_NO,  
-    KC_NO,    KC_NO,    KC_MSEL,  KC_NO,    KC_WSCH,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_MAIL,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_MPLY,  
+    KC_NO,    KC_NO,   HYPR(KC_S),KC_NO,   HYPR(KC_F),KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,   HYPR(KC_N),KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_MPLY,  
     KC_NO,    KC_NO,    KC_NO,                                  KC_NO,                        KC_VOLD,  KC_VOLU,  KC_MPRV,  KC_MNXT),
 
     // SystemLayer
     [_SL] = LAYOUT_60_ansi_split_backspace(
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-    KC_NO,SGUI(KC_WHOM),KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  
-    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  
+    KC_NO,   HYPR(KC_Q),KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,   HYPR(KC_L),KC_NO,    KC_NO,    KC_NO,  
     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    TG(_BF),  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  
     KC_NO,    KC_NO,    KC_NO,                                  RESET,                        KC_NO,    KC_NO,    KC_NO,    KC_NO),
 
