b80f05bfbf4690c3378af4f04d9f9f8ca38115e5
nguyenvukhang <brew4k@gmail.com>
2024-04-08 11:47:32 +0800

    Right: CMD with left

--- khang/dz60/keymap.c
+++ khang/dz60/keymap.c
@@ -22,6 +22,7 @@ enum keyboard_layers { BASE_L, FUNC_L, SYSTEM_L, DANGER_L };
 #define MO_SYS MO(SYSTEM_L)
 #define MO_FN MO(FUNC_L)
 #define XXXXXXX KC_NO
+#define ZZ_LEFT RGUI_T(KC_LEFT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
@@ -30,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
         KC_ESC , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,          KC_ENT ,
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, MO_FN  , _______,
-        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC ,                            KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT
+        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC ,                            ZZ_LEFT, KC_DOWN, KC_UP  , KC_RIGHT
     ),
 
     // https://docs.qmk.fm/#/keycodes
